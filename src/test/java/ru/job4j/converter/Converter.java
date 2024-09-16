c1138990608a80691c9606542ee27e899d23da3a
ivanvelikij <eldarvaleev01@gmail.com>
2024-09-16 15:31:24 +0300

    Модульные тесты [#505277]

---
+++ src/test/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,14 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float rubles) {
+        float euroRate = 90;  // Курс рубля к евро
+        return rubles / euroRate;
+    }
+
+    public static float rubleToDollar(float rubles) {
+        float dollarRate = 75;  // Курс рубля к доллару
+        return rubles / dollarRate;
+    }
+}
