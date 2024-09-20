2989af6791135aa9562d6f6ecab6bb35b153784f
ivanvelikij <eldarvaleev01@gmail.com>
2024-09-20 16:24:15 +0300

    Простое число (оператор прерывания break) [#505214]

--- src/test/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,12 +5,13 @@ public class CheckPrimeNumber {
         if (number <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(number); i++)
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
-        return true;
-    }
+        }
+            return true;
+        }
 
     public static void main(String[] args) {
         System.out.println(check(5));
