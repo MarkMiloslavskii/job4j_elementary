9f6417b9112ea2c34c5f7db891bf2a3df1d8acc0
ivanvelikij <eldarvaleev01@gmail.com>
2024-09-23 14:57:40 +0300

    Классический поиск перебором [#505227]

---
+++ src/test/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int element) {
+        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+         int i = indexOf(new int[] {5, 10, 3}, 5);
+         System.out.println(i);
+        int j = indexOf(new int[] {5, 12, 3}, 10);
+         System.out.println(j);
+        int k = indexOf(new int[] {3, 2, 1}, 1);
+        System.out.println(k);
+    }
+}
