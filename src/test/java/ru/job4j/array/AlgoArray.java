e614b61729be440742a92cafd077c848ea7e7d62
ivanvelikij <eldarvaleev01@gmail.com>
2024-09-23 15:26:08 +0300

    Упорядочить массив [#505228]

---
+++ src/test/java/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int temp = array[1]; /* переменна€ дл€ временного хранени€ значение €чейки с индексом 0. */
+        array[1] = array[2]; /* записываем в €чейку с индексом 0 значение €чейки с индексом 3. */
+        array[2] = array[0];
+        array[0] = array [3];
+        array[3] = array [4];
+        array[4] = array [2];
+        array[2] = temp;
+
+        for (int index : array) {
+            System.out.println(index);
+        }
+    }
+}
