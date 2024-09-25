f5ec4d88d52f625b84ef74613d4abf5a2374b7b0
ivanvelikij <eldarvaleev01@gmail.com>
2024-09-25 13:49:51 +0300

    Слово начинается с ... [#505233 #514364]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
-            if (!(word[i] == prefix[i])) {
+            if (word[i] != prefix[i]) {
                 result = false;
                 break;
             }
